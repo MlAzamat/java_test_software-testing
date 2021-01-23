bc01ef9a16d2cd9c0047c64b0c079be25e05cf8f
MlAzamat <33450014+MlAzamat@users.noreply.github.com>
2021-01-23 13:11:32 +0300

    THE BEST 1. Class this

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,29 @@
 package ru.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-		System.out.println("Hello______!!!");
+		hello("Hello");
+
+    	Square s = new Square(4);
+		System.out.println("Square = " + area(s));
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Rectangle = " + area(r));
+
+    	hello("Bye");
+
 	}
+
+	public static void hello(String somebody) {
+		System.out.println(somebody + " ______ " + "!!!");
+	}
+	public static double area(Square s){
+    	return s.l * s.l;
+	}
+	public static double area(Rectangle r){
+		return r.a * r.b;
+	}
+
 }
