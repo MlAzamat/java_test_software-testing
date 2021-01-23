bc481325cf9c2e32c9391e6122ed5e2715fbab24
MlAzamat <33450014+MlAzamat@users.noreply.github.com>
2021-01-23 21:05:23 +0300

    hz

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,25 +1,19 @@
 package ru.stqa.pft.sandbox;
 
-import java.awt.*;
-
 public class MyFirstProgram {
     public static void main(String[] args) {
 		hello("Hello");
+		Square s = new Square(5);
+		System.out.println("Square " + s.l + " = " + s.area());
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Rectangle " + r.a + " and " + r.b + " = " + r.area());
 
-    	Square s = new Square(4);
-		System.out.println("Square = " + s.area());
-
-		Rectangle r = new Rectangle(4, 6);
-		System.out.println("Rectangle = " + r.area());
-
-    	hello("Bye");
+		hello("Bye");
 
 	}
 
 	public static void hello(String somebody) {
 		System.out.println(somebody + " ______ " + "!!!");
 	}
-
-
 
 }
